41055f8483c2fac6054e1a745a5579d2101b35ea
lequerica <lequerica@Martins-MacBook-Pro.local>
2018-12-04 08:49:09 -0300

    add something

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+ new Recipe('nombre','Descripcion','https://learnenglishteens.britishcouncil.org/sites/teens/files/a_recipe.jpg')
+//new Recipe('nombre 2','Descripcion2','http://cdn1.tmbi.com/TOH/Images/Photos/37/1200x1200/exps25157_FM153592B03_18_12b.jpg')
+  ];
 
   constructor() { }
 
